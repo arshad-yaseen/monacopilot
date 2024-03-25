38c46c721333fbd9631d43db2b12759ca72cdef1
Arshad Yaseen <arshadpyaseen@gmail.com>
2024-03-25 16:53:57 +0530

    release: 0.2.0

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "release",
+      ],
+    ],
+    "subject-case": [0],
+  },
 }
---
+++ packages/tour/CHANGELOG.md
@@ -0,0 +1,7 @@
+# next-tour
+
+## 0.2.0
+
+### Minor Changes
+
+- New setup
