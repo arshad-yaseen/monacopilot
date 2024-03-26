30b689a86335f465356c90b8558f1e7c7dc014b5
Arshad Yaseen <arshadpyaseen@gmail.com>
2024-03-26 22:45:15 +0530

    chore: variable name change

--- packages/tour/CHANGELOG.md
+++ packages/tour/CHANGELOG.md
@@ -1,5 +1,11 @@
 # next-tour
 
+## 0.2.3
+
+### Patch Changes
+
+- Step Popvers
+
 ## 0.2.2
 
 ### Patch Changes
--- packages/tour/src/utils/positioning.ts
+++ packages/tour/src/utils/positioning.ts
@@ -1,17 +1,17 @@
 import { Position, Rect } from "../types"
 
 /**
- * Get the floating position based on the trigger and floating element
- * @param triggerRect - The trigger element rect
+ * Get the floating position based on the target and floating element
+ * @param targetRect - The target element rect
  * @param floatingRect - The floating element rect
  * @param preferredPosition - The preferred position
  */
 const getFloatingPosition = (
-  triggerRect: Rect,
+  targetRect: Rect,
   floatingRect: Rect,
   preferredPosition: Position
 ): { top: number; left: number } => {
-  const spacing = 10 // Spacing from the trigger element
+  const spacing = 10 // Spacing from the target element
   const viewPortMargin = 10 // Margin from the viewport edges
 
   let top = 0
@@ -19,20 +19,20 @@ const getFloatingPosition = (
 
   switch (preferredPosition) {
     case "top-center":
-      top = triggerRect.top - floatingRect.height - spacing
-      left = triggerRect.left + triggerRect.width / 2 - floatingRect.width / 2
+      top = targetRect.top - floatingRect.height - spacing
+      left = targetRect.left + targetRect.width / 2 - floatingRect.width / 2
       break
     case "bottom-center":
-      top = triggerRect.top + triggerRect.height + spacing
-      left = triggerRect.left + triggerRect.width / 2 - floatingRect.width / 2
+      top = targetRect.top + targetRect.height + spacing
+      left = targetRect.left + targetRect.width / 2 - floatingRect.width / 2
       break
     case "left-center":
-      top = triggerRect.top + triggerRect.height / 2 - floatingRect.height / 2
-      left = triggerRect.left - floatingRect.width - spacing
+      top = targetRect.top + targetRect.height / 2 - floatingRect.height / 2
+      left = targetRect.left - floatingRect.width - spacing
       break
     case "right-center":
-      top = triggerRect.top + triggerRect.height / 2 - floatingRect.height / 2
-      left = triggerRect.left + triggerRect.width + spacing
+      top = targetRect.top + targetRect.height / 2 - floatingRect.height / 2
+      left = targetRect.left + targetRect.width + spacing
       break
   }
 
