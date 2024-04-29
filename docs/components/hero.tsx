3c28df06b03c44a86c8c8a9ccac753fe7e4b5851
Arshad Yaseen <arshadpyaseen@gmail.com>
2024-04-29 16:47:02 +0530

    Update docs landing

--- docs/components/hero.tsx
+++ docs/components/hero.tsx
@@ -1,66 +1,65 @@
-import EditorDemo from "@/components/editor-demo";
-import { buttonVariants } from "@/components/ui/button";
-import Snippet from "@/components/ui/snippet";
-import { cn } from "@/utils/misc";
-import { motion } from "framer-motion";
-import Link from "next/link";
+import Link from 'next/link';
+
+import EditorDemo from '@/components/editor-demo';
+import {buttonVariants} from '@/components/ui/button';
+import Snippet from '@/components/ui/snippet';
+import {GITHUB_REPO_URL} from '@/constants';
+import {cn} from '@/utils/misc';
+import {GitHubLogoIcon} from '@radix-ui/react-icons';
+import {motion} from 'framer-motion';
 
 const Hero = () => {
   return (
     <section className="w-full flex flex-col items-center container">
       <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="md:text-6xl text-5xl font-semibold tracking-tighter text-center relative"
-      >
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{duration: 1}}
+        className="md:text-6xl text-5xl font-semibold tracking-tighter text-center relative">
         Github Copilot <br />
         for Web
       </motion.h1>
       <motion.p
-        initial={{ opacity: 0, y: 6 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, delay: 0.2 }}
-        className="text-muted-foreground mt-4 mb-6 text-center"
-      >
+        initial={{opacity: 0, y: 6}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 0.4, delay: 0.2}}
+        className="text-muted-foreground mt-4 mb-6 text-center">
         Extended Monaco Editor with AI auto-completion and new themes.
       </motion.p>
       <motion.div
-        initial={{ opacity: 0, y: 6 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, delay: 0.4 }}
-        className="flex gap-2"
-      >
+        initial={{opacity: 0, y: 6}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 0.4, delay: 0.4}}
+        className="flex gap-2">
         <Link
           href="/docs"
           className={cn(
             buttonVariants({
-              variant: "default",
+              variant: 'default',
             }),
-            "rounded-full"
-          )}
-        >
+            'rounded-full',
+          )}>
           Get Started
         </Link>
         <Link
-          href="/themes"
+          href={GITHUB_REPO_URL}
+          target="_blank"
           className={cn(
             buttonVariants({
-              variant: "outline",
+              variant: 'outline',
             }),
-            "rounded-full"
-          )}
-        >
-          Explore Themes
+            'rounded-full gap-2',
+          )}>
+          <GitHubLogoIcon />
+          Github
         </Link>
       </motion.div>
       <EditorDemo />
       <motion.div
-        initial={{ opacity: 0, y: 6 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, delay: 0.8 }}
-        className="w-full flex items-center justify-center"
-      >
+        initial={{opacity: 0, y: 6}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 0.4, delay: 0.8}}
+        className="w-full flex items-center justify-center">
         <Snippet value="npm install monacopilot" className="sm:w-fit w-full" />
       </motion.div>
     </section>
