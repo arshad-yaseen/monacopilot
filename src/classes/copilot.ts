3eb15c6c02e309017802e928d6807538ffde5f3c
arshad <arshadpyaseen@gmail.com>
2024-06-11 09:14:29 +0530

    Remove unused console.log

--- src/classes/copilot.ts
+++ src/classes/copilot.ts
@@ -66,22 +66,6 @@ class Copilot {
         ],
       };
 
-      console.log(
-        JSON.stringify({
-          model: COMPLETION_MODEL_IDS[model],
-          messages: [
-            {
-              role: 'system',
-              content: generateSystemPrompt(completionMetadata),
-            },
-            {
-              role: 'user',
-              content: generateUserPrompt(completionMetadata),
-            },
-          ],
-        }),
-      );
-
       const headers = {
         Authorization: `Bearer ${this.apiKey}`,
         'Content-Type': 'application/json',
