716121349ca894adcf815ff54b0dddc1a1191c72
arshad <arshadpyaseen@gmail.com>
2024-06-21 16:01:23 +0530

    Export `loader`

--- src/index.ts
+++ src/index.ts
@@ -7,24 +7,23 @@ import type {Endpoint, Technologies, Theme} from './types/copilot-editor-props';
 // Export everything from `@monaco-editor/react` except `Editor`, `EditorProps`, and `Theme`
 // We have our own `Editor` as `CopilotEditor` and `EditorProps` as `CopilotEditorProps`
 // We have our own `Theme` exported.
-export type {
-  BeforeMount,
-  DiffBeforeMount,
+export {
+  type BeforeMount,
+  type DiffBeforeMount,
+  type DiffEditorProps,
+  type DiffOnMount,
+  type Monaco,
+  type MonacoDiffEditor,
+  type OnChange,
+  type OnMount,
+  type OnValidate,
   DiffEditor,
-  DiffEditorProps,
-  DiffOnMount,
-  Monaco,
-  MonacoDiffEditor,
-  OnChange,
-  OnMount,
-  OnValidate,
   useMonaco,
   loader,
 } from '@monaco-editor/react';
 
-export {CopilotEditor as default};
-
 export {
+  CopilotEditor as default,
   CopilotEditorProps,
   Theme,
   Endpoint,
