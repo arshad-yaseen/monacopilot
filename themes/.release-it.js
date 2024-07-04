de17345d10b924472890d124606789cb0ea0ae93
arshad <arshadpyaseen@gmail.com>
2024-07-04 12:16:56 +0530

    Update themes release-it config

--- themes/.release-it.js
+++ themes/.release-it.js
@@ -1,17 +1,12 @@
 module.exports = {
   git: {
     commitMessage: 'Bump themes v${version}',
-    tagName: 'Themes v${version}',
     requireCommits: true,
     requireCleanWorkingDir: true,
   },
   github: {
-    release: true,
     draft: true,
-    releaseName: 'Monacopilot themes v${version}',
     commitArgs: ['-S'],
-    tagArgs: ['-s'],
-    assets: ['tar/*.tgz'],
   },
   npm: {
     publish: true,
