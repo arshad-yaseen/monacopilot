f6876bcf91207ef7367ec770e012019896dfcab5
arshad <arshadpyaseen@gmail.com>
2024-08-01 16:52:08 +0530

    Update OG

--- test/src/app/page.tsx
+++ test/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import {useEffect, useState} from 'react';
 
-import {Editor} from '@monaco-editor/react';
+import Editor from '@monaco-editor/react';
 import {
   registerCopilot,
   type Monaco,
