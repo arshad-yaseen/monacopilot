122d4dd09c8c4f6acef225525dd35ae83108c996
arshad <arshadpyaseen@gmail.com>
2024-08-01 16:47:06 +0530

    Update test editor

--- test/src/app/page.tsx
+++ test/src/app/page.tsx
@@ -2,45 +2,54 @@
 
 import {useEffect, useState} from 'react';
 
-import MonacoEditor from '@monaco-editor/react';
+import Editor from '@monaco-editor/react';
 import {
   registerCopilot,
   type Monaco,
   type StandaloneCodeEditor,
 } from 'monacopilot';
 
-const LANGUAGE = 'javascript';
-const COPILOT_ENDPOINT = '/api/copilot';
-
-export default function JavaScriptEditor() {
-  const [editorInstance, setEditorInstance] =
-    useState<StandaloneCodeEditor | null>(null);
-  const [monacoInstance, setMonacoInstance] = useState<Monaco | null>(null);
+export default function Home() {
+  const [monaco, setMonaco] = useState<Monaco | null>(null);
+  const [editor, setEditor] = useState<StandaloneCodeEditor | null>(null);
 
   useEffect(() => {
-    if (!monacoInstance || !editorInstance) return;
+    if (!monaco || !editor) return;
 
-    const copilotRegistration = registerCopilot(
-      monacoInstance,
-      editorInstance,
-      {
-        endpoint: COPILOT_ENDPOINT,
-        language: LANGUAGE,
-      },
-    );
+    const copilot = registerCopilot(monaco, editor, {
+      endpoint: '/api/copilot',
+      language: 'javascript',
+    });
 
     return () => {
-      copilotRegistration.deregister();
+      copilot.deregister();
     };
-  }, [monacoInstance, editorInstance]);
+  }, [monaco, editor]);
 
   return (
-    <MonacoEditor
-      language={LANGUAGE}
-      onMount={(editor, monaco) => {
-        setEditorInstance(editor);
-        setMonacoInstance(monaco);
-      }}
-    />
+    <main className="flex min-h-screen w-screen flex-col items-center">
+      <Editor
+        language="javascript"
+        onMount={(editor, monaco) => {
+          setMonaco(monaco);
+          setEditor(editor);
+        }}
+        className="h-screen w-screen"
+        theme="vs-dark"
+        options={{
+          padding: {top: 16},
+          scrollBeyondLastColumn: 0,
+          codeLens: false,
+          minimap: {enabled: false},
+          quickSuggestions: false,
+          folding: false,
+          links: false,
+          fontSize: 17,
+          wordWrap: 'on',
+          automaticLayout: true,
+          formatOnPaste: true,
+        }}
+      />
+    </main>
   );
 }
