10c66f75dc242950e1c1e34d05784831b9d2fe8b
Arshad Yaseen <arshadpyaseen@gmail.com>
2024-09-07 20:37:39 +0530

    chore(examples/nextjs): Bump monacopilot

--- examples/nextjs/app/app/api/copilot/route.ts
+++ examples/nextjs/app/app/api/copilot/route.ts
@@ -1,11 +1,11 @@
 import {NextRequest, NextResponse} from 'next/server';
 
-import {Copilot} from 'monacopilot';
+import {Copilot, type CompletionRequestBody} from 'monacopilot';
 
 const copilot = new Copilot(process.env.GROQ_API_KEY!);
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  const body: CompletionRequestBody = await req.json();
   const completion = await copilot.complete({
     body,
   });
--- examples/nextjs/pages/pages/api/copilot.ts
+++ examples/nextjs/pages/pages/api/copilot.ts
@@ -8,7 +8,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const completion = await copilot.complete(req.body);
+  const completion = await copilot.complete({
+    body: req.body,
+  });
 
   res.status(200).json(completion);
 }
