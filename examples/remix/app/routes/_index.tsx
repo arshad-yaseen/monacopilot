69e8965b938ca9328fbc360c60c74866ef444c10
Arshad Yaseen <arshadpyaseen@gmail.com>
2024-09-07 20:36:41 +0530

    chore(examples): Bump monacopilot

--- examples/remix/app/routes/_index.tsx
+++ examples/remix/app/routes/_index.tsx
@@ -2,7 +2,11 @@ import {useEffect, useState} from 'react';
 
 import {Editor} from '@monaco-editor/react';
 import type {MetaFunction} from '@remix-run/node';
-import {Monaco, registerCopilot, StandaloneCodeEditor} from 'monacopilot';
+import {
+  Monaco,
+  type registerCopilot,
+  type StandaloneCodeEditor,
+} from 'monacopilot';
 
 export const meta: MetaFunction = () => {
   return [
--- examples/remix/app/routes/copilot.tsx
+++ examples/remix/app/routes/copilot.tsx
@@ -1,10 +1,10 @@
 import {json, type ActionFunctionArgs} from '@remix-run/node';
-import {Copilot} from 'monacopilot';
+import {Copilot, type CompletionRequestBody} from 'monacopilot';
 
 const copilot = new Copilot(process.env.GROQ_API_KEY!);
 
 export const action = async ({request}: ActionFunctionArgs) => {
-  const body = await request.json();
-  const completion = await copilot.complete(body);
+  const body: CompletionRequestBody = await request.json();
+  const completion = await copilot.complete({body});
   return json(completion);
 };
